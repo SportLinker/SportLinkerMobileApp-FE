744ed754a9cc94693a4c6a311f04f3efdbabb903
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2024-05-02 20:40:20 +0700

    feat: create home screen [UI]

--- screens/NewsScreen.jsx
+++ screens/NewsScreen.jsx
@@ -1,16 +1,19 @@
 // NewsScreen.js
 import React from "react";
 import { View, Text, Button, Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const NewsScreen = ({ navigation }) => {
   return (
-    <View style={{ paddingHorizontal: 10 }}>
-      <Text>News Screen</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate("HomeScreen")}
-      />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>News Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => navigation.navigate("HomeScreen")}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
