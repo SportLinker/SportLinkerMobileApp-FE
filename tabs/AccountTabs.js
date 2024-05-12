e30a51a02a11677e13b9d76b217658529e3cd2dd
votantai3070 <votantai.07092002@gmail.com>
2024-05-12 15:38:35 +0700

    *complete* UI profile

--- tabs/AccountTabs.js
+++ tabs/AccountTabs.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import AccountScreen from "../screens/AccountScreen/AccountScreen";
 import EditAccountScreen from "../screens/AccountScreen/EditAccountScreen";
+import PasswordScreen from "../screens/AccountScreen/PasswordScreen";
 
 export default function AccountTabs() {
   const Stack = createStackNavigator();
@@ -26,6 +27,14 @@ export default function AccountTabs() {
         <Stack.Screen
           name="EditAccountScreen"
           component={EditAccountScreen}
+          options={{
+            headerShown: false,
+            headerLeft: () => null,
+          }}
+        />
+        <Stack.Screen
+          name="PasswordScreen"
+          component={PasswordScreen}
           options={{
             headerShown: false,
             headerLeft: () => null,
