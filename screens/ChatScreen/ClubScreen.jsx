a97d2faff09d119d984a6334a2a1d5166ae393d1
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2024-05-12 11:15:42 +0700

    fix: conflict

--- screens/ChatScreen/ClubScreen.jsx
+++ screens/ChatScreen/ClubScreen.jsx
@@ -1,22 +1,10 @@
 import { View, Text, SafeAreaView } from "react-native";
 import React, { useState } from "react";
-import SearchInputDynamic from "../../component/SearchInputDynamic";
-import { useFocusEffect, useRoute } from "@react-navigation/native";
 
 export default function ClubScreen() {
-  const route = useRoute();
-  const [activeScreenName, setActiveScreenName] = useState(route.name);
-
-  // Update activeScreenName whenever the screen focus changes
-  useFocusEffect(
-    React.useCallback(() => {
-      setActiveScreenName(route.name);
-    }, [route])
-  );
-
   return (
     <SafeAreaView>
-      <SearchInputDynamic screen={activeScreenName} />
+      <Text>Ninh</Text>
     </SafeAreaView>
   );
 }
---
+++ utils/constant.js
@@ -0,0 +1,33 @@
+export const sports = [
+  {
+    label: "Bóng đá",
+    value: "soccer",
+    icon: "soccer",
+  },
+  {
+    label: "Bóng rổ",
+    value: "basketball",
+    icon: "basketball",
+  },
+  {
+    label: "Bóng chuyền",
+    value: "volleyball",
+    icon: "volleyball",
+  },
+  {
+    label: "Cầu lông",
+    value: "badminton",
+    icon: "badminton",
+  },
+  {
+    label: "Bơi lội",
+    value: "swimming",
+    icon: "swim",
+  },
+  {
+    label: "Đua xe",
+    value: "racing",
+    icon: "bike",
+  },
+  // Add more sports as needed
+];
