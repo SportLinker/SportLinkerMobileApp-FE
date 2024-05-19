4b2bc6f0ebb2421e189513a87b34dc55a43b5114
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2024-05-19 21:10:55 +0700

    feat: add slider for image

---
+++ screens/SearchScreen/EventScreen/FilterEventOptionList.jsx
@@ -0,0 +1,56 @@
+import { FlatList } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Button } from "react-native-paper";
+
+const fakeData = [
+  {
+    id: 1,
+    label: "Khoảng cách",
+  },
+  {
+    id: 2,
+    label: "Thời gian",
+  },
+  {
+    id: 3,
+    label: "Thời gian",
+  },
+  {
+    id: 4,
+    label: "Thời gian",
+  },
+];
+
+const FilterEventOptionList = () => {
+  return (
+    <FlatList
+      data={fakeData}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={{ backgroundColor: "#F7F7F7" }}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item, index, separators }) => (
+        <Button
+          textColor="white"
+          mode="outlined"
+          labelStyle={{
+            marginVertical: 0,
+            marginHorizontal: 0,
+          }}
+          style={{
+            marginHorizontal: 5,
+            marginVertical: 10,
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            height: 40,
+            backgroundColor: "#707070",
+          }}
+        >
+          {item.label}
+        </Button>
+      )}
+    />
+  );
+};
+
+export default FilterEventOptionList;
