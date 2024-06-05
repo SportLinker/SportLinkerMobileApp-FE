e3bf2ff7c88b1641fae1c61bf9ddc03fcc57ce1a
Phạm Bá Phước <phuoc.18112002@gmail.com>
2024-06-05 21:12:05 +0700

    FEAT: API-Get event list

---
+++ component/Loading.jsx
@@ -0,0 +1,37 @@
+// Loading.js
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { ActivityIndicator, Text } from "react-native-paper";
+
+const Loading = ({ visible, message }) => {
+  if (!visible) return null;
+
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator animating={true} size="large" color="#1646A9" />
+      {message && <Text style={styles.message}>{message}</Text>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.4)",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 9999,
+  },
+  message: {
+    marginTop: 20,
+    fontSize: 18,
+    color: "#1646A9",
+  },
+});
+
+export default Loading;
--- screens/SearchScreen/EventScreen/EventSchedule.jsx
+++ screens/SearchScreen/EventScreen/EventSchedule.jsx
@@ -2,10 +2,14 @@ import { ScrollView, View } from "react-native";
 import FilterEventOptionList from "./FilterEventOptionList";
 import EventScheduleTable from "./EventScheduleTable";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as Location from "expo-location";
 import { getEventList } from "../../../redux/slices/eventSlice";
 import { DEFAULT_DISTACNCE } from "../../../utils/constant";
+import {
+  getEventListSelector,
+  getEventLoadingtSelector,
+} from "../../../redux/selectors";
 
 const EventSchedule = () => {
   const dispatch = useDispatch();
@@ -13,9 +17,28 @@ const EventSchedule = () => {
   const [address, setAddress] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
+  const eventListSelecter = useSelector(getEventListSelector);
+  const eventLoadingtSelector = useSelector(getEventLoadingtSelector);
+
+  const getEventListAPI = () => {
+    try {
+      const formData = {
+        long: address.longitude,
+        lat: address.latitude,
+        distance: DEFAULT_DISTACNCE,
+        start_time: 0,
+        end_time: 23,
+        sport_name: "",
+      };
+      dispatch(getEventList(formData));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    console.log("call api get event list");
-    (async () => {
+    console.log("get permission");
+    const getLocationPermission = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
@@ -27,33 +50,41 @@ const EventSchedule = () => {
 
       if (location) {
         console.log("latitude: ", location.coords.latitude);
-        console.log("latitude: ", location.coords.longitude);
+        console.log("longitude: ", location.coords.longitude);
         const userLocation = {
           latitude: location.coords.latitude,
           longitude: location.coords.longitude,
         };
-        setAddress(userLocation);
+        if (userLocation) {
+          setAddress(userLocation);
+        }
       }
-    })().then((res) => {
-      console.log("Get location done!");
-      const formData = {
-        long: address.longitude,
-        lat: address.latitude,
-        distance: DEFAULT_DISTACNCE,
-        start_time: 0,
-        end_time: 23,
-        sport_name: "Bóng đá",
-      };
-      dispatch(getEventList(formData));
-    });
+    };
+    getLocationPermission();
   }, []);
+
+  useEffect(() => {
+    // Set a timeout to execute the logic after 500 milliseconds
+    const getListTimeOut = setTimeout(() => {
+      if (address != null) {
+        console.log("adress updated");
+        console.log("address: ", address);
+        getEventListAPI();
+      }
+    }, 500);
+
+    return () => clearTimeout(getListTimeOut);
+  }, [address]);
   return (
     <View style={{ display: "flex", flexDirection: "column" }}>
       <View style={{ height: "10%" }}>
         <FilterEventOptionList />
       </View>
       <View style={{ height: "90%" }}>
-        <EventScheduleTable />
+        <EventScheduleTable
+          loading={eventLoadingtSelector}
+          eventListData={eventListSelecter}
+        />
       </View>
     </View>
   );
