f8e444b15db80fdf2e022a8c445adbd2f94491a1
votantai3070 <votantai.07092002@gmail.com>
2024-06-19 15:51:38 +0700

    FIX: Create Stadium

--- screens/YardOwner/StadiumScreen/CreateStadium/CreateStadium.jsx
+++ screens/YardOwner/StadiumScreen/CreateStadium/CreateStadium.jsx
@@ -7,7 +7,10 @@ import Step1 from "./Step/Step1";
 import Step2 from "./Step/Step2";
 import Step3 from "./Step/Step3";
 import { useDispatch } from "react-redux";
-import { createStadium } from "../../../../redux/slices/bookingSlice";
+import {
+  createStadium,
+  getStadiumByOwner,
+} from "../../../../redux/slices/bookingSlice";
 
 const fetchLocationData = async (query, setIsLoading, setLocations) => {
   setIsLoading(true);
@@ -82,7 +85,10 @@ const CreateStadium = () => {
 
   const handleCreateStadium = () => {
     console.log(stadiumData);
-    dispatch(createStadium(stadiumData)).then(() => navigation.goBack());
+    dispatch(createStadium(stadiumData)).then(() => {
+      dispatch(getStadiumByOwner());
+      navigation.goBack();
+    });
   };
 
   const nextStep = () => {
