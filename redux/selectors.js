45057082413ec85b880af2a74a62570e4a2e45a4
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2024-07-17 15:20:44 +0700

    feat: call api get list players

--- redux/selectors.js
+++ redux/selectors.js
@@ -3,6 +3,8 @@ export const getUserSelector = (state) => state.userSlice.userInfo;
 export const getUserLoadingSelector = (state) => state.userSlice.loading;
 
 // EVENT
+
+export const getAllPlayersSelector = (state) => state.eventSlice.userList;
 export const getEventSelector = (state) => state.eventSlice.event;
 export const getEventListSelector = (state) => state.eventSlice.eventList;
 export const getMyEventListSelector = (state) => state.eventSlice.myEventList;
