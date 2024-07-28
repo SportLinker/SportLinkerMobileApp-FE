5400e5c86c09c92ee35f7108e0538512f976b453
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2024-07-28 19:39:15 +0700

    setting env

--- services/api.js
+++ services/api.js
@@ -3,9 +3,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API_URL } from "@env";
 
 export const api = axios.create({
-  baseURL: "https://be.sportlinker.site/v1/api",
+  // baseURL: "https://be.sportlinker.site/v1/api",
   // baseURL: "https://fine-special-ram.ngrok-free.app/v1/api",
-  // baseURL: API_URL,
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use(
--- services/socket.js
+++ services/socket.js
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
+import { API_SOCKET } from "@env";
 
-const socket = io("https://fine-special-ram.ngrok-free.app"); // Replace with your server URL
+const socket = io(API_SOCKET); // Replace with your server URL
 
 export default socket;
